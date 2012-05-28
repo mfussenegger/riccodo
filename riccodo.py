e9279adb71c1252c1ee74820c6595db3b765394d
Mathias Fussenegger <f.mathias@zignar.net>
2012-05-28 21:16:52 +0200

    changed path handling

--- riccodo.py
+++ riccodo.py
@@ -5,15 +5,18 @@ import os
 from markdown import Markdown
 
 from jinja2 import Environment, FileSystemLoader
-from jinja2.exceptions import TemplateNotFound
+
+PATH_CONTENT = '../site/content/'
+PATH_TEMPLATES = '../site/templates/'
+
 
 class MarkdownReader(object):
     METADATA = {
-        'template' : lambda x: str(x).strip(),
-        'sort:' : lambda x: str(x).strip(),
-        'title' : lambda x: str(x).strip(),
-        'parent:' : lambda x: str(x).strip(),
-        'in_nav' : lambda x: True if x == '1' or x == 'true' else False
+        'template': lambda x: str(x).strip(),
+        'sort:': lambda x: str(x).strip(),
+        'title': lambda x: str(x).strip(),
+        'parent:': lambda x: str(x).strip(),
+        'in_nav': lambda x: True if x == '1' or x == 'true' else False
     }
     extensions = ['codehilite', 'extra']
 
@@ -23,6 +26,12 @@ class MarkdownReader(object):
         return value
 
     def read(self, filename):
+        """Parses the given file and returns a :class:`Page` object
+
+        :param filename: path to the file to read.
+        :type filename: str.
+        :returns: Page
+        """
         text = open(filename, 'r', encoding='utf-8').read()
         md = Markdown(extensions=set(self.extensions + ['meta']))
         content = md.convert(text)
@@ -50,6 +59,7 @@ class Page(object):
                  sort,
                  in_nav):
         self.path = path
+        self.url = get_url(self.path) + '.html'
         self.name = os.path.basename(path)
         self.title = title or self.name
         self.content = content
@@ -70,12 +80,13 @@ def get_pages():
     mdr = MarkdownReader()
     pages = []
 
-    for root, dirs, files in os.walk('./content'):
+    for root, dirs, files in os.walk(PATH_CONTENT):
         for fi in files:
             path = os.path.join(root, fi)
             pages.append(mdr.read(path))
 
     return pages
+
 
 def build_page_tree(pages):
     parent_names = set([x.parent_name for x in pages
@@ -89,8 +100,10 @@ def build_page_tree(pages):
 
     return pages, pages_flat
 
+
 def get_url(path):
-    return '../{0}'.format(path.replace('/content', ''))
+    return '..{0}'.format(path.replace(PATH_CONTENT, ''))
+
 
 def in_path(page, children):
     if page in children:
@@ -100,16 +113,18 @@ def in_path(page, children):
             return in_path(page, child.children)
     return False
 
+
 def write_html(page_tree, pages_flat):
-    env = Environment(loader=FileSystemLoader('./templates'))
+    env = Environment(loader=FileSystemLoader(PATH_TEMPLATES))
     env.globals['get_url'] = get_url
     env.globals['in_path'] = in_path
     for page in pages_flat:
         template = env.get_template(page.template)
-        content = template.render({ 'page' : page, 'pages' : page_tree })
-        path = '.{0}.html'.format(page.path.replace('content', 'output'))
+        content = template.render({'page': page, 'pages': page_tree})
+        path = '.{0}.html'.format(page.path.replace(PATH_CONTENT, 'output'))
         with open(path, 'w', encoding='utf-8') as f:
             f.write(content)
+
 
 def main():
     pages = get_pages()
